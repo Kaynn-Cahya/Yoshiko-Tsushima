b29a765862169e83a7d09608717ad3fcbffe449d
Yeo-Wen-Qin <KaynnCahya@outlook.com>
2021-02-05 18:19:58 +0900

    Login to bot via token on start

---
+++ YoshikoBot/Yoshiko-Tsushima/Models/Credentials.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YoshikoBot.Models {
+    internal class Credentials {
+        public string Token { get; set; }
+    }
+}
--- YoshikoBot/Yoshiko-Tsushima/Program.cs
+++ YoshikoBot/Yoshiko-Tsushima/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
 
-namespace Yoshiko_Tsushima
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+using YoshikoBot.Models;
+
+namespace YoshikoBot {
+	public class Program {
+
+        private string CredentialsFilePath {
+            get { 
+                return Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "Credentials", "Token.json");
+            }
+        }
+
+        private DiscordSocketClient client;
+
+        public static void Main(string[] args)
+			=> new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync() {
+            client = new DiscordSocketClient();
+
+            string token = JsonConvert.DeserializeObject<Credentials>(File.ReadAllText(CredentialsFilePath)).Token;
+
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            await Task.Delay(-1);
         }
     }
 }
